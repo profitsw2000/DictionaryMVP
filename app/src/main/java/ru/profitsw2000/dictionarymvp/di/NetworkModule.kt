dff5e705ec3dc9158c40506995f627394d4f08fc
profitsw2000 <profitsw2000@gmail.com>
2022-06-07 22:19:13 +0300

    Добавлен модуль NetworkModule.kt для внедрения зависимостей с помощью Dagger2.

--- app/src/main/java/ru/profitsw2000/dictionarymvp/di/NetworkModule.kt
+++ app/src/main/java/ru/profitsw2000/dictionarymvp/di/NetworkModule.kt
@@ -1,4 +1,64 @@
 package ru.profitsw2000.dictionarymvp.di
 
+import dagger.Module
+import dagger.Provides
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.profitsw2000.dictionarymvp.data.RepositoryImpl
+import ru.profitsw2000.dictionarymvp.data.local.DataSourceLocal
+import ru.profitsw2000.dictionarymvp.data.web.ApiService
+import ru.profitsw2000.dictionarymvp.data.web.DataSourceRemote
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
 class NetworkModule {
+    @Provides
+    @Named("api_url")
+    fun provideBaseUrl(): String {
+        return "https://dictionary.skyeng.ru/api/public/v1/"
+    }
+
+    @Provides
+    fun provideConverterFactory(): Converter.Factory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(@Named("api_url") baseUrl: String, converterFactory: Converter.Factory): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(converterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRemoteDataSource(apiService: ApiService): DataSourceRemote {
+        return DataSourceRemote()
+    }
+
+    @Singleton
+    @Provides
+    fun provideLocalDataSource(): DataSourceLocal {
+        return DataSourceLocal()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRepository(dataSourceRemote: DataSourceRemote,
+                          dataSourceLocal: DataSourceLocal): RepositoryImpl {
+        return RepositoryImpl(dataSourceRemote, dataSourceLocal)
+    }
+
 }
