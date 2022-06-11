2e16e52f00cf065a22c541d1e178e1b7689e0b9b
profitsw2000 <profitsw2000@gmail.com>
2022-06-11 14:40:45 +0300

    Создан файл с субмодулями для внедрения зависимостей с помощью библиотеки Koin.

---
+++ app/src/main/java/ru/profitsw2000/dictionarymvp/di/KoinModules.kt
@@ -0,0 +1,37 @@
+package ru.profitsw2000.dictionarymvp.di
+
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.profitsw2000.dictionarymvp.data.RepositoryImpl
+import ru.profitsw2000.dictionarymvp.data.entities.DataModel
+import ru.profitsw2000.dictionarymvp.data.local.DataSourceLocal
+import ru.profitsw2000.dictionarymvp.data.web.ApiService
+import ru.profitsw2000.dictionarymvp.data.web.DataSourceRemote
+import ru.profitsw2000.dictionarymvp.domain.DataSource
+import ru.profitsw2000.dictionarymvp.domain.Repository
+import ru.profitsw2000.dictionarymvp.ui.main.MainInteractor
+import ru.profitsw2000.dictionarymvp.ui.main.MainViewModel
+
+val webModule = module {
+    single<String>(named(URL)) {"https://dictionary.skyeng.ru/api/public/v1/"}
+    single { Retrofit.Builder()
+        .baseUrl(get<String>(named(URL)))
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .addConverterFactory(get())
+        .build() }
+    single<ApiService> { get<Retrofit>().create(ApiService::class.java)}
+    single<DataSource<List<DataModel>>>(named(NAME_REMOTE)) { DataSourceRemote(get())}
+    single<DataSource<List<DataModel>>>(named(NAME_LOCAL)) { DataSourceLocal() }
+    single<Repository<List<DataModel>>>(named(NAME_REPO)) { RepositoryImpl(get(named(NAME_REMOTE)), get(named(NAME_LOCAL))) }
+
+    factory { MainInteractor(get(named(NAME_REPO))) }
+    factory<Converter.Factory> { GsonConverterFactory.create() }
+}
+
+val appModule = module {
+    factory { MainViewModel(get()) }
+}
