49e79abc504b97bba341d96edaf8171c32e629e2
profitsw2000 <profitsw2000@gmail.com>
2022-06-11 21:10:10 +0300

    Добавлена функция в интерфейс ApiService.kt для асинхронного запроса с использованием корутин.

--- app/src/main/java/ru/profitsw2000/dictionarymvp/data/web/ApiService.kt
+++ app/src/main/java/ru/profitsw2000/dictionarymvp/data/web/ApiService.kt
@@ -1,6 +1,7 @@
 package ru.profitsw2000.dictionarymvp.data.web
 
 import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Query
 import ru.profitsw2000.dictionarymvp.data.entities.DataModel
@@ -8,4 +9,7 @@ import ru.profitsw2000.dictionarymvp.data.entities.DataModel
 interface ApiService {
     @GET("words/search")
     fun search(@Query("search") wordToSearch: String): Single<List<DataModel>>
+
+    @GET("words/search")
+    fun asyncSearch(@Query("search") wordToSearch: String): Deferred<List<DataModel>>
 }
