95779a9950bfa1a29bb49fdc99386beecfff9783
profitsw2000 <profitsw2000@gmail.com>
2022-06-11 21:28:53 +0300

    Изменена функция getData (suspend) и тип возвращаемого ею значения в интерфейсе Repository.kt и его имплементации RepositoryImpl.kt.

--- app/src/main/java/ru/profitsw2000/dictionarymvp/data/RepositoryImpl.kt
+++ app/src/main/java/ru/profitsw2000/dictionarymvp/data/RepositoryImpl.kt
@@ -4,14 +4,13 @@ import io.reactivex.rxjava3.core.Single
 import ru.profitsw2000.dictionarymvp.data.entities.DataModel
 import ru.profitsw2000.dictionarymvp.data.local.DataSourceLocal
 import ru.profitsw2000.dictionarymvp.data.web.DataSourceRemote
-import ru.profitsw2000.dictionarymvp.domain.DataSource
 import ru.profitsw2000.dictionarymvp.domain.Repository
 
 class RepositoryImpl(
     private val dataSourceRemote: DataSourceRemote,
     private val dataSourceLocal: DataSourceLocal)
     : Repository<List<DataModel>> {
-    override fun getData(word: String, remoteRepository: Boolean): Single<List<DataModel>> {
+    override suspend fun getData(word: String, remoteRepository: Boolean): List<DataModel> {
         return if(remoteRepository) {
             dataSourceRemote.getData(word)
         } else {
--- app/src/main/java/ru/profitsw2000/dictionarymvp/domain/Repository.kt
+++ app/src/main/java/ru/profitsw2000/dictionarymvp/domain/Repository.kt
@@ -3,5 +3,5 @@ package ru.profitsw2000.dictionarymvp.domain
 import io.reactivex.rxjava3.core.Single
 
 interface Repository<T> {
-    fun getData(word: String, remoteRepository: Boolean): Single<T>
+    suspend fun getData(word: String, remoteRepository: Boolean): T
 }
