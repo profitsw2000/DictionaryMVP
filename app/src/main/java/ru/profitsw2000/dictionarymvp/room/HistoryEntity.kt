4634caa1b2acb234ff4b96a05b8aea573c7e3c10
profitsw2000 <profitsw2000@gmail.com>
2022-06-19 15:00:55 +0300

    Добавлены классы и интерфейсы для создания БД Room и таблиц для него.

---
+++ app/src/main/java/ru/profitsw2000/dictionarymvp/room/HistoryEntity.kt
@@ -0,0 +1,14 @@
+package ru.profitsw2000.dictionarymvp.room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(indices = arrayOf(Index(value = arrayOf("word"), unique = true)))
+class HistoryEntity(
+    @field:PrimaryKey
+    @field:ColumnInfo(name = "word") var word: String,
+    @field:ColumnInfo(name = "translation") var translation: String?,
+    @field:ColumnInfo(name = "note") var note: String?
+)
